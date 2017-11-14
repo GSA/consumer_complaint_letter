d3ec7a31ad2952f21d7c760dec018c8b1ed2bf9e
Dan Narkiewicz <dnarkiewicz@ctacorp.com>
2017-11-14 16:09:47 -0500

    use secure cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CahRails::Application.config.session_store :cookie_store, key: '_cah_rails_session'
+CahRails::Application.config.session_store :cookie_store, key: '_cah_rails_session', :secure => true, :httponly => true
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
